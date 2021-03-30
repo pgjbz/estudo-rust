69eb209e7c02f1a4f4eb9e4810bb83578344dc25
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-03-29 22:58:18 -0300

    tuple and array improve

--- no-primitive-types/src/main.rs
+++ no-primitive-types/src/main.rs
@@ -1,13 +1,13 @@
-
+use std::io;
 
 fn main() {
     println!("Tuple!");
-    let tuple: (i8, &str, u16);
-    tuple = (4, "Banana", 65000);
-    println!("Tuple is a \"array\" with different types: {}, {}, {}", tuple.0, tuple.1, tuple.2);
+    let tup: (i8, &str, u16) = (4, "Banana", 65000);
+    let (x, y, z) = tup;
+    println!("Tuple is a \"array\" with different types: {} - {} - {}", x, y, z);
     println!("Array types!");
     println!("Stack alloc array");
-    let array: [u8; 5] = [255,1,2,3,4];
+    let array: [u8; 5] = [0; 5];
     println!("Stack array value in pos 0: {}", array[0]);
     println!("Heap array");
     let heap_array: Box<[i32]> = Box::new([45]);
@@ -21,4 +21,30 @@ fn main() {
     for x in 0..vect.len() {
         println!("{}", x);
     }
+
+    input_text();
+}
+
+fn input_text(){
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
