50c98e8f2eb72096056a424bbbe842208cf6e49a
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-03-30 21:58:44 -0300

    Loops

---
+++ loops/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Loop for");
+    for i in 0..10 {
+        print!(" - {} - ", i);
+    }
+    println!("\nLoop for each");
+
+    let arr: [i8; 5] = [0; 5];
+
+    for i in arr.iter() {
+        print!(" - {} - ", i);
+    }
+
+    println!("\nLoop while");
+
+    let mut i: i8 = 0;
+
+    while i < 10 {
+        i+=1;
+        println!("I value {}", i);
+    }
+
+    println!("Do while???");
+    i = 0;
+
+    loop {
+        i+=1;
+        println!("I value {}", i);
+        if i >= 10 { break; }
+    }
+
+}
