84ea90d58f9e66f11ffcb271808e4c55c42adc5c
Paulo Gabriel <pgj1085@gmail.com>
2021-08-19 15:23:47 -0300

    Add Copy trait to largest function to compile it

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,17 +1,17 @@
 use std::ops::Deref;
 
-// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> T {
-//
-// 	let mut largest = list.get(0).unwrap();
-//
-// 	for item in list.iter() {
-// 		if item > largest {
-// 			largest = &item;
-// 		}
-// 	}
-//
-// 	largest
-// }
+fn largest<T: std::cmp::PartialOrd + Copy>(list: &[T]) -> T {
+
+	let mut largest = list[0];
+
+	for &item in list {
+		if item > largest {
+			largest = item;
+		}
+	}
+
+	largest
+}
 
 struct Point<T> {
 	x: T,
@@ -47,15 +47,15 @@ impl<T, U> PointTwo<T, U> {
 }
 
 fn main() {
-	// let number_list = vec![34, 50, 25, 100, 65];
+	let number_list = vec![34, 50, 25, 100, 65];
 
-	// let result = largest(&number_list);
-	// println!("The largest number is {}", result);
-	//
-	// let char_list = vec!['y', 'm', 'a', 'q'];
-	//
-	// let result = largest(&char_list);
-	// println!("The largest char is {}", result);
+	let result = largest(&number_list);
+	println!("The largest number is {}", result);
+
+	let char_list = vec!['y', 'm', 'a', 'q'];
+
+	let result = largest(&char_list);
+	println!("The largest char is {}", result);
 
 	let integer = Point::<i32> { x: 5, y: 10 };
 	let float = Point::<f32> { x: 1.0, y: 4.0 };
