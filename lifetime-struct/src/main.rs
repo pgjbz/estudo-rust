5cef9a30fec637fc3ed5b1e4e63ca47d45afbb30
Paulo Gabriel <pgj1085@gmail.com>
2021-08-23 16:39:43 -0300

    Lifetime in struct and generic with lifetimes

---
+++ lifetime-struct/src/main.rs
@@ -0,0 +1,47 @@
+use std::fmt::Display;
+
+#[derive(Default)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+    where
+        T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    println!("{}", first_sentence);
+    println!("{}", i.part);
+    println!("Level: {}", i.level());
+    println!("Part: {}", i.announce_and_return_part("i go to pep"));
+    longest_with_an_announcement(novel.as_str(), first_sentence, "banana");
+}
