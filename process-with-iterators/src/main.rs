1b7361aee6faa723f6459a8c06250694c5ae0196
Paulo Gabriel Justino Bezerra <paulo.bezerra.ext@sascar.com.br>
2021-09-13 11:58:13 -0300

    Creating a iter and use next

--- process-with-iterators/src/main.rs
+++ process-with-iterators/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+
+    let v1 = vec![1,2,3];
+    let mut v1_iter = v1.iter(); //create a iter
+
+    // for n in v1_iter {
+    //     println!("Value {}", n);
+    // }
+
+    assert_eq!(v1_iter.next(), Some(&1)); //to use next iter have to be mutable
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None)
 }
