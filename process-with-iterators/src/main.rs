2ff74bf18efe26eb5ed35d1effd3649383c1c8ca
Paulo Gabriel Justino Bezerra <paulo.bezerra.ext@sascar.com.br>
2021-09-13 13:19:53 -0300

    Use map and filter in iterator

--- process-with-iterators/src/main.rs
+++ process-with-iterators/src/main.rs
@@ -3,12 +3,77 @@ fn main() {
     let v1 = vec![1,2,3];
     let mut v1_iter = v1.iter(); //create a iter
 
-    // for n in v1_iter {
-    //     println!("Value {}", n);
-    // }
+    for (i, n) in v1_iter.clone().enumerate() { //close because for take borrow
+        println!("Got {} in index {}", n, i);
+    }
 
     assert_eq!(v1_iter.next(), Some(&1)); //to use next iter have to be mutable
     assert_eq!(v1_iter.next(), Some(&2));
     assert_eq!(v1_iter.next(), Some(&3));
-    assert_eq!(v1_iter.next(), None)
+    assert_eq!(v1_iter.next(), None);
+
+    let v2 = vec![1,2,3];
+    let v2_vec: Vec<_> = v2.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2_vec, vec![2, 3, 4]);
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
 }
