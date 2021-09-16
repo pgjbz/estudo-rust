a8d4f91f76846448cd2515ecb04473a016ea2e87
Paulo Gabriel <pgj1085@gmail.com>
2021-09-16 13:09:17 -0300

    Drop trait and manual  drop call

--- smart-points-traits/src/main.rs
+++ smart-points-traits/src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::Deref;
+use std::{fmt::{Debug, Display}, ops::Deref};
 
 fn main() {
     let a = 1;
@@ -19,21 +19,38 @@ fn main() {
     let d = MyBox::new(String::from("Test"));
     hello(&d); //Implicit Deref Coercions
     hello(&(*d)[..]); //Derref if not have implicit deref
+
+    // Drop references in inverse order of creation
+
+    println!("Creating...");
+    let droped = MyBox::new(String::from("Hello world!"));
+    drop(droped); //free memory
+    println!("Manual drop executed");
 }
 
-struct MyBox<T>(T);
+struct MyBox<T>(T)
+ where T: Debug + Display;
 
-impl<T> MyBox<T> {
+impl<T> MyBox<T>
+where T: Debug  + Display{
     fn new(x:T) -> MyBox<T> {
         MyBox(x)
     }
 }
 
-impl<T> Deref for MyBox<T> {
+impl<T> Deref for MyBox<T>
+where T: Debug + Display{
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+impl<T> Drop for MyBox<T>
+where T: Debug + Display {
+    fn drop(&mut self) { //canot call directly
+        println!("Drop with value {}", self.0)
     }
 }
 
