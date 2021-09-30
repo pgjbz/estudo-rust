8d2388f9e89e32934d3a440e9860ce02336221de
Paulo Gabriel <pgj1085@gmail.com>
2021-09-30 18:17:02 -0300

    Using thred::spawn to create new thread

---
+++ using-threads/src/main.rs
@@ -0,0 +1,27 @@
+use std::{thread, time::Duration};
+
+fn main() {
+    let handle = thread::spawn(||{
+        for i in 0..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 0..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+    
+    handle.join().unwrap(); //similar to join thread in Java
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+
+}
+
